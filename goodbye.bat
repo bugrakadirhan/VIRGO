2f5b6dc4d0b14b3023283c61ab3e6061874b7b00
Buğra Kadirhan <42012971+bugrakadirhan@users.noreply.github.com>
2019-06-20 02:16:24 +0300

    2019-06-20T02:16:23+0300

--- goodbye.bat
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-CALL FINISH.BAT
-SET T=%TIME:~0,2%
-SET T=%T%:%TIME:~3,2%
-SET T=%T%:%TIME:~6,2%
-SET T=%T: =0%
-ECHO Shutted the computer down at %T% >> finishes/FAREWELL.LOG
-SHUTDOWN -s -f -t 20 -c "Goodnight..."
